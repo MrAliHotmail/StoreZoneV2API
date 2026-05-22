edee2c8ab502e6a19ab48d203e5d0885b17a0e4d
Ali Alzubaidi <57499275+MrAli911@users.noreply.github.com>
2026-05-22 16:36:49 +0300

    Edit/store zone v2 api.sln/file (#36)
    * Update Sort Floder in "BuildingBlocks"Add CRUD categories (#33)
    
    Monoloth DDD
    
    * update this file:storeZoneV2API.sln

--- StoreZoneV2API.Application/Services/CategoreyService.cs
+++ StoreZoneV2API.Application/Services/CategoreyService.cs
@@ -1,10 +1,42 @@
-﻿using StoreZoneV2API.Domain.Entities;
-using StoreZoneV2API.Domain.Interfaces.Data;
+﻿using StoreZoneV2API.Domain.Interfaces.Data;
 
 namespace StoreZoneV2API.Application.Services
 {
-    public class CategoreyService
+    public class CategoreyService 
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public CategoreyService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IEnumerable<Category?>> GetAllCategoriesAsync()
+        {
+            return await _unitOfWork.Categories.GetAllAsync();
+        }
+        public async Task<Category?> GetCategoryByIdAsync(int id)
+        {
+            return await _unitOfWork.Categories.GetByIdAsync(id);
+        }
+        public async Task AddCategoryAsync(Category category)
+        {
+            await _unitOfWork.Categories.AddAsync(category);
+            await _unitOfWork.SaveChangesAsync();
+        }
+        public async Task UpdateCategoryAsync(Category category)
+        {
+            await  _unitOfWork.Categories.UpdateAsync(category);
+            await _unitOfWork.SaveChangesAsync();
+        }
+        public async Task DeleteCategoryAsync(int id)
+        {
+            var category = await _unitOfWork.Categories.GetByIdAsync(id);
+            if (category != null)
+            {
+                await  _unitOfWork.Categories.DeleteAsync(id);
+                await _unitOfWork.SaveChangesAsync();
+            }
+        }
+
     }
 
 }
